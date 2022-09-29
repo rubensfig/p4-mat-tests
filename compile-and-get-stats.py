6b13a3de21f472c9e1b7e562460e7cff1836d0d7
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2022-09-29 12:58:19 +0200

    add script

--- compile-and-get-stats.py
+++ compile-and-get-stats.py
@@ -5,16 +5,18 @@ import json
 import pandas as pd
 
 COMPILE_COMMAND = "bf-p4c --display-power-budget -g "
-TCAM_FLAG = "-D TCAM "
-SIZES = [1, 64, 128, 512, 1024, 2048, 4096]
-SIZE_FLAG = "-D TBL_SIZE_"
-PROGRAM = " mat-tests.p4"
+TCAM_FLAG = " -D TCAM "
+TBL_SIZES = [1, 64, 128, 512, 1024, 2048, 4096]
+WDTH_SIZES = [32, 64, 128, 512]
+SIZE_FLAG = " -D TBL_SIZE_"
+WDTH_FLAG = " -D WDTH_"
+PROGRAM = " mat-tests-k-wdth.p4"
 
 pwr_df = pd.DataFrame(columns=["table_size", "type", "gress", "power"])
-for i in SIZES:
-    command = COMPILE_COMMAND + SIZE_FLAG + str(i) + PROGRAM
+for i in WDTH_SIZES:
+    command = COMPILE_COMMAND + SIZE_FLAG + str(1024) + WDTH_FLAG + str(i) + PROGRAM
     print(command)
-    # os.system(COMPILE_COMMAND + SIZE_FLAG )
+    os.system(command)
 
     data = None
     with open("mat-tests.tofino/pipe/logs/power.json") as f:
@@ -30,4 +32,4 @@ for i in SIZES:
                 ignore_index=True
     )
      
-pwr_df.to_csv("power_table_size.csv")
+pwr_df.to_csv("power_table_size_k_wdth.csv")
