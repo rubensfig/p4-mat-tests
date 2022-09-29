8e48d2984dffd8869d46c0806ce1e0257904f749
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2022-09-29 09:54:05 +0200

    fix compile

--- compile-and-get-stats.py
+++ compile-and-get-stats.py
@@ -4,14 +4,15 @@ import os
 import json
 import pandas as pd
 
-COMPILE_COMMAND = "bf-p4c mat-tests.p4 --display-power-budget -g "
+COMPILE_COMMAND = "bf-p4c --display-power-budget -g "
 TCAM_FLAG = "-D TCAM "
 SIZES = [1, 64, 128, 512, 1024, 2048, 4096]
 SIZE_FLAG = "-D TBL_SIZE_"
+PROGRAM = " mat-tests.p4"
 
 pwr_df = pd.DataFrame(columns=["table_size", "type", "gress", "power"])
 for i in SIZES:
-    command = COMPILE_COMMAND + SIZE_FLAG + str(i)
+    command = COMPILE_COMMAND + SIZE_FLAG + str(i) + PROGRAM
     print(command)
     # os.system(COMPILE_COMMAND + SIZE_FLAG )
 
@@ -29,5 +30,4 @@ for i in SIZES:
                 ignore_index=True
     )
      
-
 pwr_df.to_csv("power_table_size.csv")
